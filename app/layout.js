76bb83f281fef5559a8d6699a819e8edd6452a2a
Akmalmhd <akmalmhdofficial@gmail.com>
2025-03-15 13:08:58 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
+});
+
+const ovo = Ovo({
+  subsets: ["latin"], weight: ["400"]
+});
+
+export const metadata = {
+  title: "Akmal Muhammed",
+  description: "Graphic Designer in Kerala",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body
+        className={`${outfit.className} ${ovo.className} antialiased overflow-x-hidden dark:bg-darkTheme dark:text-white`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
